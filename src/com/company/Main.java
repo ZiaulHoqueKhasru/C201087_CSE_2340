dd432886b4d16522419dc3182ab25429fccfd922
ZiaulHoqueKhasru <www.mzhk@gmail.com>
2021-08-02 09:55:32 +0600

    main file be commited

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+//Name: Md.Ziaul Hoque
+//id: C201087
+//section:3CM
